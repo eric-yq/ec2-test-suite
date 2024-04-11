6736a52a8a30abc02d2e39d0128d0ef4e04edf1a
eric-yq <yuanquan8261@163.com>
2024-04-11 10:15:59 +0800

    change

--- samples-of-submit-benchmark/submit-benchmark-mysql-increased-vuser_2.sh
+++ samples-of-submit-benchmark/submit-benchmark-mysql-increased-vuser_2.sh
@@ -2,8 +2,8 @@
 
 ## 待测 EC2 规格和 OS
 os_types="al2023"
-# instance_types="r5.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge"
-instance_types="r5.2xlarge"
+instance_types="r5.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge"
+# instance_types="r5.2xlarge"
 
 for os in ${os_types} 
 do
@@ -20,14 +20,14 @@ do
 		echo "$0: Star to run benchmark"
 		source /tmp/temp-setting
 		
-		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  1 1
-# 		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  2 64
-# 	    bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  4 64
-# 		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  6 64
-# 		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  8 64
-# 		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60 10 64
-# 		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60 12 64
-# 		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60 16 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  1 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  2 64
+	    bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  4 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  6 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60  8 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60 10 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60 12 64
+		bash benchmark/mysql-benchmark_v2.sh ${INSTANCE_IP_MASTER} 60 16 64
 		
 		## 停止实例
 		aws ec2 stop-instances --instance-ids ${INSTANCE_ID} 
