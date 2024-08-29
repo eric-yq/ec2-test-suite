a320b3f9097661947465a58d62066b7a3ad79ebf
eric-yq <yuanquan8261@163.com>
2024-08-29 20:34:33 +0800

    change

--- benchmark/qdrant-bechmark.sh
+++ benchmark/qdrant-bechmark.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 用法： bash qdrant-benchmark <IP地址> <EC2规格>
+# 用法： bash qdrant-benchmark <IP地址>
 
+source /tmp/temp-setting
 
 ## 需要进行 benchmark 的 SUT 主机
 if [[ X${1} == X"" ]]; then
@@ -9,8 +10,6 @@ if [[ X${1} == X"" ]]; then
 else
     HOST="--host $1"
 fi
-
-INS_TYPE=${2}
 
 ##########################################################################################
 # 启动测试: Search benchmark
@@ -20,7 +19,9 @@ INS_TYPE=${2}
 ## --datasets： 在 datasets/datasets.json 文件中搜索 "name"
 ## 参考官方结果的数据集： https://qdrant.tech/benchmarks/#tested-datasets 
 cd /root/vector-db-benchmark/
-DATASET_SEARCH="dbpedia-openai-1M-1536-angular deep-image-96-angular gist-960-euclidean glove-100-angular"
+# DATASET_SEARCH="dbpedia-openai-1M-1536-angular deep-image-96-angular gist-960-euclidean glove-100-angular"
+DATASET_SEARCH="glove-100-angular"
+
 cat << EOF > test-search.sh
 for i in $DATASET_SEARCH
 do
@@ -36,7 +37,8 @@ EOF
 ## --datasets： 在 datasets/datasets.json 文件中搜索 "name"
 ## 参考官方结果的数据集： https://qdrant.tech/benchmarks/filter-result-2023-02-03.json
 cd /root/vector-db-benchmark/
-DATASET_FILTER="100-kw-small-vocab-filters  100-kw-small-vocab-no-filters  arxiv-titles-384-filters  arxiv-titles-384-no-filters  geo-radius-100-filters  geo-radius-100-no-filters  geo-radius-2048-filters  geo-radius-2048-no-filters  h-and-m-2048-filters  h-and-m-2048-no-filters  int-100-filters  int-100-no-filters  int-2048-filters  int-2048-no-filters  keyword-100-filters  keyword-100-no-filters  keyword-2048-filters  keyword-2048-no-filters  range-100-filters  range-100-no-filters  range-2048-filters  range-2048-no-filters"
+# DATASET_FILTER="100-kw-small-vocab-filters  100-kw-small-vocab-no-filters  arxiv-titles-384-filters  arxiv-titles-384-no-filters  geo-radius-100-filters  geo-radius-100-no-filters  geo-radius-2048-filters  geo-radius-2048-no-filters  h-and-m-2048-filters  h-and-m-2048-no-filters  int-100-filters  int-100-no-filters  int-2048-filters  int-2048-no-filters  keyword-100-filters  keyword-100-no-filters  keyword-2048-filters  keyword-2048-no-filters  range-100-filters  range-100-no-filters  range-2048-filters  range-2048-no-filters"
+DATASET_FILTER="100-kw-small-vocab-filters"
 cat << EOF > test-filter.sh
 for i in $DATASET_FILTER
 do
@@ -54,21 +56,21 @@ bash test-filter.sh
 
 ##########################################################################################
 ## 结果文件处理
-RESULT_FILE_PATH="/root/vector-db-benchmark/results_$INS_TYPE"
+RESULT_FILE_PATH="/root/vector-db-benchmark/results_$INSTANCE_TYPE"
 mkdir -p $RESULT_FILE_PATH
 mv /root/vector-db-benchmark/*.json $RESULT_FILE_PATH/
 # 合并数据
 cd $RESULT_FILE_PATH/
-jq -s '.' *upload*.json > qdrant_benchmark_upload_$INS_TYPE.json
-jq -s '.' *search*.json > qdrant_benchmark_search_$INS_TYPE.json
+jq -s '.' *upload*.json > qdrant_benchmark_upload_$INSTANCE_TYPE.json
+jq -s '.' *search*.json > qdrant_benchmark_search_$INSTANCE_TYPE.json
 # qdrant_benchmark_search_m6i.2xlarge.json 
 # qdrant_benchmark_upload_m6i.2xlarge.json
 ## 结果文件生成 csv 文件：upload 文件
-echo "experiment,engine,dataset,parallel,batch_size,hnsw_config.m,hnsw_config.ef_construct,upload_time,total_time" > qdrant_benchmark_upload_$INS_TYPE.csv
-jq -r '.[] | "\(.params.experiment),\(.params.engine),\(.params.dataset),\(.params.parallel),\(.params.batch_size),\(.params.hnsw_config.m),\(.params.hnsw_config.ef_construct),\(.results.upload_time),\(.results.total_time)"' qdrant_benchmark_upload_$INS_TYPE.json >> qdrant_benchmark_upload_$INS_TYPE.csv
+echo "experiment,engine,dataset,parallel,batch_size,hnsw_config.m,hnsw_config.ef_construct,upload_time,total_time" > qdrant_benchmark_upload_$INSTANCE_TYPE.csv
+jq -r '.[] | "\(.params.experiment),\(.params.engine),\(.params.dataset),\(.params.parallel),\(.params.batch_size),\(.params.hnsw_config.m),\(.params.hnsw_config.ef_construct),\(.results.upload_time),\(.results.total_time)"' qdrant_benchmark_upload_$INSTANCE_TYPE.json >> qdrant_benchmark_upload_$INSTANCE_TYPE.csv
 ## 结果文件生成 csv 文件：search 文件
-echo "experiment,engine,dataset,parallel,hnsw_ef,oversampling,mean_precisions,rps,total_time,mean_time,p95_time,p99_time" > qdrant_benchmark_search_$INS_TYPE.csv
-jq -r '.[] | "\(.params.experiment),\(.params.engine),\(.params.dataset),\(.params.parallel),\(.params.config.hnsw_ef),\(.params.config.quantization.oversampling),\(.results.mean_precisions),\(.results.rps),\(.results.total_time),\(.results.mean_time),\(.results.p95_time),\(.results.p99_time)"' qdrant_benchmark_search_$INS_TYPE.json >> qdrant_benchmark_search_$INS_TYPE.csv
+echo "experiment,engine,dataset,parallel,hnsw_ef,oversampling,mean_precisions,rps,total_time,mean_time,p95_time,p99_time" > qdrant_benchmark_search_$INSTANCE_TYPE.csv
+jq -r '.[] | "\(.params.experiment),\(.params.engine),\(.params.dataset),\(.params.parallel),\(.params.config.hnsw_ef),\(.params.config.quantization.oversampling),\(.results.mean_precisions),\(.results.rps),\(.results.total_time),\(.results.mean_time),\(.results.p95_time),\(.results.p99_time)"' qdrant_benchmark_search_$INSTANCE_TYPE.json >> qdrant_benchmark_search_$INSTANCE_TYPE.csv
 cd ../
-tar czf qdrant_benchmark_result_$INS_TYPE.tar.gz $RESULT_FILE_PATH/
-cp qdrant_benchmark_result_$INS_TYPE.tar.gz /home/ec2-user/
+tar czf qdrant_benchmark_result_$INSTANCE_TYPE.tar.gz $RESULT_FILE_PATH/
+cp qdrant_benchmark_result_$INSTANCE_TYPE.tar.gz /home/ec2-user/
