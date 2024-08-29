92e02da649e213b217d137a22a8155618710ffa7
eric-yq <yuanquan8261@163.com>
2024-08-29 16:55:09 +0800

    change

--- benchmark/qdrant-bechmark.sh
+++ benchmark/qdrant-bechmark.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# 用法： bash qdrant-benchmark <IP地址> <EC2规格>
+
+
 ## 需要进行 benchmark 的 SUT 主机
 if [[ X${1} == X"" ]]; then
 	HOST=""
 else
     HOST="--host $1"
 fi
+
+INS_TYPE=${2}
+
 ##########################################################################################
 # 启动测试: Search benchmark
 # python3 -m run --engines XXX --datasets XXX
@@ -48,13 +54,11 @@ bash test-filter.sh
 
 ##########################################################################################
 ## 结果文件处理
-INS_TYPE=$(cloud-init query ds.meta_data.instance_type)
 RESULT_FILE_PATH="/root/vector-db-benchmark/results_$INS_TYPE"
 mkdir -p $RESULT_FILE_PATH
 mv /root/vector-db-benchmark/*.json $RESULT_FILE_PATH/
 # 合并数据
 cd $RESULT_FILE_PATH/
-INS_TYPE=$(cloud-init query ds.meta_data.instance_type)
 jq -s '.' *upload*.json > qdrant_benchmark_upload_$INS_TYPE.json
 jq -s '.' *search*.json > qdrant_benchmark_search_$INS_TYPE.json
 # qdrant_benchmark_search_m6i.2xlarge.json 
@@ -63,99 +67,8 @@ jq -s '.' *search*.json > qdrant_benchmark_search_$INS_TYPE.json
 echo "experiment,engine,dataset,parallel,batch_size,hnsw_config.m,hnsw_config.ef_construct,upload_time,total_time" > qdrant_benchmark_upload_$INS_TYPE.csv
 jq -r '.[] | "\(.params.experiment),\(.params.engine),\(.params.dataset),\(.params.parallel),\(.params.batch_size),\(.params.hnsw_config.m),\(.params.hnsw_config.ef_construct),\(.results.upload_time),\(.results.total_time)"' qdrant_benchmark_upload_$INS_TYPE.json >> qdrant_benchmark_upload_$INS_TYPE.csv
 ## 结果文件生成 csv 文件：search 文件
-INS_TYPE=$(cloud-init query ds.meta_data.instance_type)
 echo "experiment,engine,dataset,parallel,hnsw_ef,oversampling,mean_precisions,rps,total_time,mean_time,p95_time,p99_time" > qdrant_benchmark_search_$INS_TYPE.csv
 jq -r '.[] | "\(.params.experiment),\(.params.engine),\(.params.dataset),\(.params.parallel),\(.params.config.hnsw_ef),\(.params.config.quantization.oversampling),\(.results.mean_precisions),\(.results.rps),\(.results.total_time),\(.results.mean_time),\(.results.p95_time),\(.results.p99_time)"' qdrant_benchmark_search_$INS_TYPE.json >> qdrant_benchmark_search_$INS_TYPE.csv
 cd ../
 tar czf qdrant_benchmark_result_$INS_TYPE.tar.gz $RESULT_FILE_PATH/
 cp qdrant_benchmark_result_$INS_TYPE.tar.gz /home/ec2-user/
-
-## upload 文件
-#   {
-#     "params": {
-#       "experiment": "qdrant-sq-rps-m-64-ef-512",
-#       "engine": "qdrant",
-#       "dataset": "dbpedia-openai-1M-1536-angular",
-#       "parallel": 16,
-#       "batch_size": 1024,
-#       "optimizers_config": {
-#         "max_segment_size": 1000000,
-#         "memmap_threshold": 10000000,
-#         "default_segment_number": 2
-#       },
-#       "hnsw_config": {
-#         "m": 64,
-#         "ef_construct": 512
-#       },
-#       "quantization_config": {
-#         "scalar": {
-#           "type": "int8",
-#           "quantile": 0.99,
-#           "always_ram": true
-#         }
-#       }
-#     },
-#     "results": {
-#       "post_upload": {},
-#       "upload_time": 320.67010107799433,
-#       "total_time": 1343.5359594210022
-#     }
-#   }
-#
-## search 文件
-#   {
-#     "params": {
-#       "dataset": "dbpedia-openai-1M-1536-angular",
-#       "experiment": "qdrant-sq-rps-m-64-ef-512",
-#       "engine": "qdrant",
-#       "parallel": 1,
-#       "config": {
-#         "hnsw_ef": 64,
-#         "quantization": {
-#           "rescore": true,
-#           "oversampling": 1.0
-#         }
-#       }
-#     },
-#     "results": {
-#       "total_time": 14.071015573994373,
-#       "mean_time": 0.002298591131120338,
-#       "mean_precisions": 0.9929,
-#       "std_time": 0.0002850996156008218,
-#       "min_time": 0.0015700549993198365,
-#       "max_time": 0.008849541991367005,
-#       "rps": 355.3403785040825,
-#       "p95_time": 0.0027295801570289767,
-#       "p99_time": 0.0028827918137540112
-#     }
-#   }
-
-
-
-
-### 附录：https://qdrant.tech/benchmarks/results-1-100-thread-2024-06-15.json 中的格式
-#   {
-#     "engine_name": "qdrant",
-#     "setup_name": "qdrant-sq-rps-m-64-ef-512",
-#     "dataset_name": "dbpedia-openai-1M-1536-angular",
-#     "upload_time": 211.02451519703027,
-#     "total_upload_time": 1466.1785857389914,
-#     "p95_time": 0.003128117803134956,
-#     "rps": 319.1132749085913,
-#     "parallel": 1,
-#     "p99_time": 0.003539099645131501,
-#     "mean_time": 0.0025649284036204337,
-#     "mean_precisions": 0.9915200000000001,
-#     "engine_params": {
-#       "hnsw_ef": 64,
-#       "quantization": {
-#         "rescore": true,
-#         "oversampling": 2
-#       }
-#     }
-#   }
-#
-### 通过 jq 命令将这个文件转换为 csv 文件：
-# jq -r '.[] | "\(.engine_name), \(.setup_name), \(.dataset_name), \(.upload_time), \(.total_upload_time), \(.parallel), \(.engine_params.hnsw_ef), \(.engine_params.quantization.oversampling), \(.rps), \(.mean_precisions), \(.mean_time), \(.p95_time), \(.p99_time)"' results-1-100-thread-2024-06-15.json > results-1-100-thread-2024-06-15-new.csv
-
-
