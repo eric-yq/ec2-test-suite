04d1e6e857e2c523ad72a11eabba2520ff15b6b1
eric-yq <yuanquan8261@163.com>
2025-02-27 15:32:43 +0800

    change

--- benchmark/valkey-benchmark_v1.sh
+++ benchmark/valkey-benchmark_v1.sh
@@ -19,7 +19,7 @@ THREAD_LIST="2 4 6 8 10 12 16"
 for i in ${THREAD_LIST}
 do
 # 	redis-cli -h ${SUT_IP_ADDR} flushall
-	RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}-${i}.txt"
+	RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${SUT_IP_ADDR}-${i}.txt"
 	
 	OPTS="-t ${i} -c 4"
 	
@@ -27,6 +27,8 @@ do
 	  --pipeline 10 --distinct-client-seed \
 	  --key-pattern=R:R --key-prefix=TEST \
 	  --random-data --data-size-range=1-512 --data-size-pattern=S  \
-	  −−randomize --hide-histogram --run-count=3 --ratio=1:5 \
+	  --hide-histogram --run-count=3 --ratio=1:5 \
 	  --out-file=${RESULT_FILE}
 done
+
+# −−randomize 
