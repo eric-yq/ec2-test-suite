1f61980ac0c7efc307a93fb435679f3d60973679
eric-yq <yuanquan8261@163.com>
2025-02-28 17:28:57 +0800

    change

--- launch-instances-cluster.sh
+++ launch-instances-cluster.sh
@@ -91,6 +91,20 @@ if [[ ${SUT_NAME} == "redis-cluster" ]] || \
         echo "export INSTANCE_IP_SLAVE1=${INSTANCE_IP_SLAVE1}" >> /tmp/temp-setting
         echo "export INSTANCE_IP_SLAVE2=${INSTANCE_IP_SLAVE2}" >> /tmp/temp-setting
         
+        ## 输出公网IP地址
+        INSTANCE_PUBLIC_IP_MASTER=$(terraform output -raw instance_public_ip_0)
+        INSTANCE_PUBLIC_IP_MASTER2=$(terraform output -raw instance_public_ip_01)
+        INSTANCE_PUBLIC_IP_MASTER2=$(terraform output -raw instance_public_ip_02)
+        INSTANCE_PUBLIC_IP_SLAVE=$(terraform output -raw instance_public_ip_1)
+        INSTANCE_PUBLIC_IP_SLAVE1=$(terraform output -raw instance_public_ip_11)
+        INSTANCE_PUBLIC_IP_SLAVE2=$(terraform output -raw instance_public_ip_12)
+        echo "export INSTANCE_PUBLIC_IP_MASTER=${INSTANCE_PUBLIC_IP_MASTER}" >> /tmp/temp-setting
+        echo "export INSTANCE_PUBLIC_IP_MASTER1=${INSTANCE_PUBLIC_IP_MASTER1}" >> /tmp/temp-setting
+        echo "export INSTANCE_PUBLIC_IP_MASTER2=${INSTANCE_PUBLIC_IP_MASTER2}" >> /tmp/temp-setting
+        echo "export INSTANCE_PUBLIC_IP_SLAVE=${INSTANCE_PUBLIC_IP_SLAVE}" >> /tmp/temp-setting
+        echo "export INSTANCE_PUBLIC_IP_SLAVE1=${INSTANCE_PUBLIC_IP_SLAVE1}" >> /tmp/temp-setting
+        echo "export INSTANCE_PUBLIC_IP_SLAVE2=${INSTANCE_PUBLIC_IP_SLAVE2}" >> /tmp/temp-setting
+        
         ## 输出实例ID，用于benchmark结束后释放实例
         INSTANCE_ID_MASTER=$(terraform output -raw instance_id_0)
         INSTANCE_ID_MASTER1=$(terraform output -raw instance_id_01)
---
+++ samples-of-submit-benchmark/submit-benchmark-valkey-cluster-test_v1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Redis Benchmark
+# 待测 EC2 规格和 OS
+os_types="al2023"
+instance_types="r6a.2xlarge r6g.2xlarge r6i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r8g.2xlarge" 
+
+for os in ${os_types} 
+do
+	for ins in ${instance_types} 
+	do
+		## 创建实例、安装软件
+		echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
+		bash launch-instances-cluster.sh -s valkey-cluster -t ${ins} -o ${os}
+
+		echo "$0: Sleep 300 seconds..."
+		sleep 300
+
+		## 执行 Benchmark 测试
+		echo "$0: Star to run benchmark"
+		source /tmp/temp-setting
+		bash benchmark/valkey-benchmark_v1.sh ${INSTANCE_IP_MASTER} 180
+		
+		## 停止实例
+		aws ec2 terminate-instances --instance-ids ${INSTANCE_ID_MASTER} ${INSTANCE_ID_MASTER1} ${INSTANCE_ID_MASTER2} ${INSTANCE_ID_SLAVE} ${INSTANCE_ID_SLAVE1} ${INSTANCE_ID_SLAVE2}
+	done
+done
+
+echo "$0: Valkey benchmark completed."
