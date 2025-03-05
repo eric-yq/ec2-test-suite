0187925df56569fe52a0ee58346aa51c3146801e
eric-yq <yuanquan8261@163.com>
2025-03-05 19:59:50 +0800

    change

--- launch-instances-nginx.sh
+++ launch-instances-nginx.sh
@@ -31,12 +31,14 @@ then
     OS_TYPE=al2
 fi
 
+
 REGION_NAME=$(cloud-init query region)
 echo "" > /tmp/temp-setting
 echo "export REGION_NAME=${REGION_NAME}" >> /tmp/temp-setting
 echo "export INSTANCE_TYPE=${INSTANCE_TYPE}" >> /tmp/temp-setting
 echo "export OS_TYPE=${OS_TYPE}" >> /tmp/temp-setting
 echo "export SUT_NAME=${SUT_NAME}" >> /tmp/temp-setting
+
 
 ## 根据实例类型、OS 类型查找最新的 AMI。
 bash search_latest_ami.sh
@@ -57,10 +59,18 @@ MAC=$(cloud-init query ds.meta_data.mac)
 SUBNET_ID_XXX=$(cloud-init query ds.meta_data.network.interfaces.macs.$MAC.subnet_id)
 SG_ID_XXX=$(cloud-init query ds.meta_data.network.interfaces.macs.$MAC.security_group_ids)
 
+# 获取placement group name
+ins_id=$(cloud-init query ds.meta_data.instance_id)
+PG_NAME_XXX=$(aws ec2 describe-instances \
+  --instance-ids $ins_id \
+  --query "Reservations[0].Instances[0].Placement.GroupName" \
+  --output text)
+
 ## 修改 variables.tf 内容 
 sed -i "s/REGION_NAME_XXX/${REGION_NAME}/g" variables.tf
 sed -i "s/SUBNET_ID_XXX/${SUBNET_ID_XXX}/g" variables.tf
 sed -i "s/SG_ID_XXX/${SG_ID_XXX}/g" variables.tf
+sed -i "s/PG_NAME_XXX/${PG_NAME_XXX}/g" variables.tf
 sed -i "s/INSTANCE_NAME_XXX/SUT_${SUT_NAME}/g" variables.tf
 sed -i "s/INSTANCE_TYPE_XXX/${INSTANCE_TYPE}/g" variables.tf
 sed -i "s/AMI_ID_XXX/${AMI_ID}/g" variables.tf
@@ -110,6 +120,7 @@ cd tf_cfg_${SUT_NAME}
 sed -i "s/REGION_NAME_XXX/${REGION_NAME}/g" variables.tf
 sed -i "s/SUBNET_ID_XXX/${SUBNET_ID_XXX}/g" variables.tf
 sed -i "s/SG_ID_XXX/${SG_ID_XXX}/g" variables.tf
+sed -i "s/PG_NAME_XXX/${PG_NAME_XXX}/g" variables.tf
 sed -i "s/INSTANCE_NAME_XXX/SUT_${SUT_NAME}/g" variables.tf
 sed -i "s/INSTANCE_TYPE_XXX/${INSTANCE_TYPE}/g" variables.tf
 sed -i "s/AMI_ID_XXX/${AMI_ID}/g" variables.tf
---
+++ samples-of-submit-benchmark/submit-benchmark-valkey-test_zhangkai.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Redis Benchmark
+# 待测 EC2 规格和 OS
+os_types="al2023"
+instance_types="r6g.xlarge r6i.xlarge r7g.xlarge r7i.xlarge r8g.xlarge r6g.2xlarge r6i.2xlarge r7g.2xlarge r7i.2xlarge r8g.2xlarge" 
+
+
+echo "$0: Valkey benchmark, single node..."
+for os in ${os_types} 
+do
+	for ins in ${instance_types} 
+	do
+		## 创建实例、安装软件
+		echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
+		bash launch-instances-single.sh -s valkey -t ${ins} -o ${os}
+		
+		echo "$0: Sleep 120 seconds..."
+		sleep 120
+		
+		## 执行 Benchmark 测试
+		echo "$0: Star to run benchmark"
+		source /tmp/temp-setting
+		bash benchmark/valkey-benchmark_zhangkai.sh ${INSTANCE_IP_MASTER} 120
+		
+		## 停止实例
+		aws ec2 terminate-instances --region $REGION_NAME --instance-ids ${INSTANCE_ID} &
+	done
+done
+
+echo "$0: Valkey benchmark, cluster ..."
+for os in ${os_types} 
+do
+	for ins in ${instance_types} 
+	do
+		## 创建实例、安装软件
+		echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
+		bash launch-instances-cluster.sh -s valkey-cluster -t ${ins} -o ${os}
+
+		echo "$0: Sleep 120 seconds..."
+		sleep 120
+
+		## 执行 Benchmark 测试
+		echo "$0: Star to run benchmark"
+		source /tmp/temp-setting
+		bash benchmark/valkey-benchmark_zhangkai.sh ${INSTANCE_IP_MASTER} 120
+		
+		## 停止实例
+		aws ec2 terminate-instances --region $REGION_NAME --instance-ids \
+		  ${INSTANCE_ID_MASTER} ${INSTANCE_ID_MASTER1} ${INSTANCE_ID_MASTER2} \
+		  ${INSTANCE_ID_SLAVE} ${INSTANCE_ID_SLAVE1} ${INSTANCE_ID_SLAVE2} &
+	done
+done
+
+echo "$0: Valkey benchmark completed."
