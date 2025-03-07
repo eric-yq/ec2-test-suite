61f0ffff4815662185f34bee5d15ed21165ec472
eric-yq <yuanquan8261@163.com>
2025-03-08 07:46:13 +0800

    change

--- samples-of-submit-benchmark/submit-benchmark-valkey-test_zhangkai.sh
+++ samples-of-submit-benchmark/submit-benchmark-valkey-test_zhangkai.sh
@@ -37,8 +37,8 @@ do
 		echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
 		bash launch-instances-cluster.sh -s valkey-cluster -t ${ins} -o ${os}
 
-		echo "$0: Sleep 180 seconds..."
-		sleep 180
+# 		echo "$0: Sleep 180 seconds..."
+# 		sleep 180
 
 		## 执行 Benchmark 测试
 		echo "$0: Star to run benchmark"
