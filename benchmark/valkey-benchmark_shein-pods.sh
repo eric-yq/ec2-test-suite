b2d9874640af5019decfa485c57a43e72bcb0622
eric-yq <yuanquan8261@163.com>
2025-03-13 16:09:55 +0800

    change

--- benchmark/valkey-benchmark_shein-pods.sh
+++ benchmark/valkey-benchmark_shein-pods.sh
@@ -8,13 +8,14 @@
 ## 场景 1
 SUT_IP_ADDR=${1}
 PORT=${2}
+DATASIZE=32
 
 source /tmp/temp-setting
 RESULT_PATH="/root/ec2-test-suite/benchmark-result-files"
 mkdir -p ${RESULT_PATH}
 
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-shein-set.txt"
-memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="set __key__ __data__" --key-prefix="kv_" --key-minimum=1 --key-maximum=500 --random-data --data-size=64 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
+memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="set __key__ __data__" --key-prefix="kv_" --key-minimum=1 --key-maximum=500 --random-data --data-size=$DATASIZE --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
 
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-shein-get.txt"
 memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="get __key__" --key-prefix="kv_" --key-minimum=1 --key-maximum=500 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
@@ -23,13 +24,13 @@ RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-
 memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="incr __key__" --key-prefix="int_" --key-minimum=1 --key-maximum=500 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
 
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-shein-lpush.txt"
-memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="lpush __key__ __data__" --key-prefix="list_" --key-minimum=1 --key-maximum=500 --random-data --data-size=64 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
+memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="lpush __key__ __data__" --key-prefix="list_" --key-minimum=1 --key-maximum=500 --random-data --data-size=$DATASIZE --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
 
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-shein-sadd.txt"
-memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="sadd __key__ __data__" --key-prefix="set_" --key-minimum=1 --key-maximum=500 --random-data --data-size=64 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
+memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="sadd __key__ __data__" --key-prefix="set_" --key-minimum=1 --key-maximum=500 --random-data --data-size=$DATASIZE --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
 
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-shein-zadd.txt"
-memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="zadd __key__ __key__ __data__" --key-prefix="" --key-minimum=1 --key-maximum=500 --random-data --data-size=64 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
+memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="zadd __key__ __key__ __data__" --key-prefix="" --key-minimum=1 --key-maximum=500 --random-data --data-size=$DATASIZE --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
 
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${SUT_IP_ADDR}_${PORT}-shein-hset.txt"
-memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="hset __key__ __data__ __data__" --key-prefix="hash_" --key-minimum=1 --key-maximum=500 --random-data --data-size=64 --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
+memtier_benchmark -t 2 -c 5 --pipeline=10 -s ${SUT_IP_ADDR} -p $PORT --distinct-client-seed --command="hset __key__ __data__ __data__" --key-prefix="hash_" --key-minimum=1 --key-maximum=500 --random-data --data-size=$DATASIZE --test-time=180 --out-file=${RESULT_FILE} --hide-histogram 
