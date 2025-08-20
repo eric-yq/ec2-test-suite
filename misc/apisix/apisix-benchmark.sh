331812dc4cb013786ea57ba15ca28eb3cd81f136
eric-yq <yuanquan8261@163.com>
2025-08-20 17:38:37 +0800

    change

--- misc/apisix/apisix-benchmark.sh
+++ misc/apisix/apisix-benchmark.sh
@@ -13,18 +13,35 @@ apt install -y build-essential unzip
 wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_$ARCH -O /usr/bin/yq
 chmod +x /usr/bin/yq
 
-# 安装 OpenResty
-apt install -y --no-install-recommends wget gnupg ca-certificates
-wget -O - https://openresty.org/package/pubkey.gpg --no-check-certificate | sudo gpg --dearmor -o /usr/share/keyrings/openresty.gpg
-if [[ $ARCH == "arm64" ]]; then
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/arm64/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
-else
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
-fi
-apt update
-apt install -y openresty
-openresty -v
+# # 安装 OpenResty
+# apt install -y --no-install-recommends wget gnupg ca-certificates
+# wget -O - https://openresty.org/package/pubkey.gpg --no-check-certificate | sudo gpg --dearmor -o /usr/share/keyrings/openresty.gpg
+# if [[ $ARCH == "arm64" ]]; then
+#     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/arm64/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
+# else
+#     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/openresty.gpg] http://openresty.org/package/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list > /dev/null
+# fi
+# apt update
+# apt install -y openresty
+# openresty -v
 
+# 安装 apisix
+cd /opt
+APISIX_VERSION='3.9.1'
+git clone --depth 1 --branch ${APISIX_VERSION} https://github.com/apache/apisix.git apisix-${APISIX_VERSION}
+cd apisix-${APISIX_VERSION}
+sed -i.bak "s/LUAROCKS_VER=3.8.0/LUAROCKS_VER=3.12.0/g" utils/linux-install-luarocks.sh
+sed -i.bak "s/lualdap = 1.2.6-1/lualdap = 1.4.0/g" apisix-master-0.rockspec 
+make deps -j && make install
+
+# 安装 etcd
+cd /opt
+ETCD_VERSION='3.5.4'
+wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-$ARCH.tar.gz
+tar -xvf etcd-v${ETCD_VERSION}-linux-$ARCH.tar.gz && \
+  cd etcd-v${ETCD_VERSION}-linux-$ARCH && \
+  cp -a etcd etcdctl /usr/bin/
+nohup etcd >/tmp/etcd.log 2>&1 &
 
 # 安装 wrk
 cd /opt
@@ -35,23 +52,8 @@ make -j
 ln -s /opt/wrk-4.2.0/wrk /usr/bin/wrk
 wrk -v
 
-# 安装 etcd
-cd /opt
-ETCD_VERSION='3.5.4'
-wget https://github.com/etcd-io/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-$ARCH.tar.gz
-tar -xvf etcd-v${ETCD_VERSION}-linux-$ARCH.tar.gz && \
-  cd etcd-v${ETCD_VERSION}-linux-$ARCH && \
-  cp -a etcd etcdctl /usr/bin/
-nohup etcd >/tmp/etcd.log 2>&1 &
-   
-# 安装 apisix
-cd /opt
-APISIX_VERSION='3.9.1'
-git clone --depth 1 --branch ${APISIX_VERSION} https://github.com/apache/apisix.git apisix-${APISIX_VERSION}
-cd apisix-${APISIX_VERSION}
-make deps -j && make install
-
 # benchmark
+cd /opt/apisix-${APISIX_VERSION}
 sed -i.bak "s/bash -x/bash/g"   ./benchmark/run.sh
 sed -i "s/wrk -d 5/wrk -d 60/g" ./benchmark/run.sh
 ulimit -n 65535
