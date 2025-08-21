f1f19ca0d5d91956558fc42521dcde79eb5189cc
eric-yq <yuanquan8261@163.com>
2025-08-21 14:02:07 +0800

    change

--- benchmark/redis-benchmark_v1.sh
+++ benchmark/redis-benchmark_v1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## 使用方法： bash redis-benchmark_v1.sh <IP地址> <执行时间(秒)>
+## 使用方法： bash redis-benchmark_v1.sh <IP地址> <端口号> <执行时间(秒)>
 
 # OPTS="-t 8 -c 64"
 # if [[ ${SUT_NAME} == "redis-cluster" ]]; then
@@ -9,7 +9,8 @@
 
 ## 场景 1
 SUT_IP_ADDR=${1}
-TEST_TIME=${2}
+SUT_PORT=${2}
+TEST_TIME=${3}
 
 source /tmp/temp-setting
 RESULT_PATH="/root/ec2-test-suite/benchmark-result-files"
@@ -21,12 +22,11 @@ THREAD_LIST="1 2 4 6 8 12 16 32 48 64"
 for i in ${THREAD_LIST}
 do
 	redis-cli -h ${SUT_IP_ADDR} flushall
-	RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}-${i}.txt"
+	RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${SUT_IP_ADDR}_${SUT_PORT}_${i}.txt"
 	
-	OPTS="-t ${i} -c 4"
+	OPTS="--threads ${i} --clients 4"
 	
-	memtier_benchmark ${OPTS} -s ${SUT_IP_ADDR} --test-time ${TEST_TIME} \
+	memtier_benchmark ${OPTS} --server ${SUT_IP_ADDR} --port ${SUT_PORT} --test-time ${TEST_TIME} \
 	  --random-data --data-size-range=1-4096 --data-size-pattern=S \
-	  --hide-histogram --run-count=3 --ratio=1:4 \
-	  --out-file=${RESULT_FILE}
+	  --hide-histogram --run-count=3 --ratio=1:4 --out-file=${RESULT_FILE}
 done
--- samples-of-submit-benchmark/submit-benchmark-redis-test_v1.sh
+++ samples-of-submit-benchmark/submit-benchmark-redis-test_v1.sh
@@ -3,7 +3,7 @@
 # Redis Benchmark
 # 待测 EC2 规格和 OS
 os_types="al2023"
-instance_types="r5.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r8g.2xlarge" 
+instance_types="r8i.2xlarge r8g.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge r5.2xlarge" 
 
 for os in ${os_types} 
 do
@@ -19,7 +19,10 @@ do
 		## 执行 Benchmark 测试
 		echo "$0: Star to run benchmark"
 		source /tmp/temp-setting
-		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 6379 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 8006 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 8010 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 8014 180
 		
 		## 停止实例
 		aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
