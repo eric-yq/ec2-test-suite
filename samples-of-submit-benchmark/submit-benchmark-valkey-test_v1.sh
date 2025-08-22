12cf36651a35e2098fc8499f72eeae6b83c4f6b0
eric-yq <yuanquan8261@163.com>
2025-08-22 16:47:56 +0800

    change

--- samples-of-submit-benchmark/submit-benchmark-valkey-test_v1.sh
+++ samples-of-submit-benchmark/submit-benchmark-valkey-test_v1.sh
@@ -3,7 +3,7 @@
 # Redis Benchmark
 # 待测 EC2 规格和 OS
 os_types="al2023"
-instance_types="r6g.2xlarge r6i.2xlarge r7g.2xlarge r7i.2xlarge r8g.2xlarge" 
+instance_types="r8i.2xlarge r8g.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge r5.2xlarge"
 
 for os in ${os_types} 
 do
@@ -13,16 +13,19 @@ do
 		echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
 		bash launch-instances-single.sh -s valkey -t ${ins} -o ${os}
 		
-		echo "$0: Sleep 120 seconds..."
-		sleep 120
-		
-		## 执行 Benchmark 测试
+		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 180 seconds ..."
+		sleep 180
+
+		## 执行 Benchmark 测试，使用 redis-benchmark_v1.sh 脚本
 		echo "$0: Star to run benchmark"
 		source /tmp/temp-setting
-		bash benchmark/valkey-benchmark_v1.sh ${INSTANCE_IP_MASTER} 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 6379 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 8003 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 8005 180
+		bash benchmark/redis-benchmark_v1.sh ${INSTANCE_IP_MASTER} 8007 180
 		
 		## 停止实例
-		aws ec2 stop-instances --region $REGION_NAME --instance-ids ${INSTANCE_ID} &
+		aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
 	done
 done
 
