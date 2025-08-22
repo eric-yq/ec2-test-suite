4bf022e18bb405dc4d5a27a94d76e96cae5f8083
eric-yq <yuanquan8261@163.com>
2025-08-22 11:23:00 +0800

    change

--- compile/07-mesos-on-c7g.sh
+++ compile/07-mesos-on-c7g.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # r7g.2xlarge(Graviton3, 8C 64G), Rocky Linux 8.10, 40G gp3
-# Rocky Linux 8.10 AMI ID 从 https://rockylinux.org/zh-CN/download 查找
-# AMI 特殊说明：上面 URL 找到的 AMI 中通过 lvs 管理分区，需要提前将 rocky-root 扩展为 EBS 的大小。
+# Rocky Linux 8.10 AMI ID 从 https://rockylinux.org/zh-CN/download 查找，
+# AMI 说明：以 us-east-2 为例，点击 Deploy 按钮之后，在 EC2 Cosole 可以看到有两种镜像：
+# 1. Rocky-8-EC2-Base-8.10-20240528.0.aarch64（ami-06660dd1be13e71a8）
+# 2. Rocky-8-EC2-LVM-8.10-20240528.0.aarch64（ami-04a7157c80d6f6930）
+# 选用第一个 Base 的镜像，不需要自己管理 lvm 逻辑卷大小。
 
 sudo su - root
 
