5bddfe51f2402a8cdd1c97eb4e7d7483feca4694
eric-yq <yuanquan8261@163.com>
2025-08-24 06:26:57 +0800

    change

--- benchmark/mongo-benchmark_v2.sh
+++ benchmark/mongo-benchmark_v2.sh
@@ -13,10 +13,10 @@ submit_task(){
 		HOSTS="${SUT_IP_ADDR}:27017"   
 		MONGO_URL=mongodb://root:gv2mongo@${HOSTS}/
 	
-	elif  [[ "$SUT_NAME" == "mongo-replicaset" ]]; then
+	# elif  [[ "$SUT_NAME" == "mongo-replicaset" ]]; then
 		
-		HOSTS="${INSTANCE_IP_MASTER}:27017,${INSTANCE_IP_SLAVE}:27017,${INSTANCE_IP_SLAVE1}:27017"
-		MONGO_URL=mongodb://root:gv2mongo@${HOSTS}/?replicaSet=rs0gv2mongo
+	# 	HOSTS="${INSTANCE_IP_MASTER}:27017,${INSTANCE_IP_SLAVE}:27017,${INSTANCE_IP_SLAVE1}:27017"
+	# 	MONGO_URL=mongodb://root:gv2mongo@${HOSTS}/?replicaSet=rs0gv2mongo
     
     else 
     	
@@ -27,11 +27,9 @@ submit_task(){
     
     ## Profile: workload_mongo
 #   THREAD_LIST="16 32 48 64 80 96 112 128 144 160 176 192"
-#     THREAD_LIST="1 2 4 6 8 12 16 32 48 64 80 96"
     THREAD_LIST="2 4 6 8 10 12 14 16 32"
-#     THREAD_LIST="1"
-    RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}-ycsb-load.txt"
-    RESULT_FILE1="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}-ycsb-run.txt"
+    RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${SUT_IP_ADDR}-ycsb-load.txt"
+    RESULT_FILE1="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${SUT_IP_ADDR}-ycsb-run.txt"
 
 	### 加载数据, load
 	echo "[Info] Load data ..." >> ${RESULT_FILE}
@@ -41,22 +39,22 @@ submit_task(){
 		
     for i in ${THREAD_LIST}
     do
-        ### 执行 Benchmark, run
-#         echo "[Info] This Test, current Thread=${i}: Run benchmark - Read only ..." >> ${RESULT_FILE1}
-#         /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_readonly -p mongodb.url=${MONGO_URL} \
-#             -threads ${i} >> ${RESULT_FILE1}
-#             
-#         echo "[Info] This Test, current Thread=${i}: Run benchmark - Update only ..." >> ${RESULT_FILE1}
-#         /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_updateonly -p mongodb.url=${MONGO_URL} \
-#             -threads ${i} >> ${RESULT_FILE1}
-            
-        echo "[Info] This Test, current Thread=${i}: Run benchmark - Mixed(R:U:I=4:4:2) ..." >> ${RESULT_FILE1}
-        /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_rui -p mongodb.url=${MONGO_URL} \
+        ## 执行 Benchmark, run
+        echo "[Info] This Test, current Thread=${i}: Run benchmark - Read only ..." >> ${RESULT_FILE1}
+        /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_readonly -p mongodb.url=${MONGO_URL} \
             -threads ${i} >> ${RESULT_FILE1}
             
-#         echo "[Info] This Test, current Thread=${i}: Run benchmark - Mixed(100% Read-Modify-Write) ..." >> ${RESULT_FILE1}
-#         /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_mixed -p mongodb.url=${MONGO_URL} \
-#             -threads ${i} >> ${RESULT_FILE1} 
+        echo "[Info] This Test, current Thread=${i}: Run benchmark - Update only ..." >> ${RESULT_FILE1}
+        /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_updateonly -p mongodb.url=${MONGO_URL} \
+            -threads ${i} >> ${RESULT_FILE1}
+            
+        # echo "[Info] This Test, current Thread=${i}: Run benchmark - Mixed(R:U:I=4:4:2) ..." >> ${RESULT_FILE1}
+        # /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_rui -p mongodb.url=${MONGO_URL} \
+        #     -threads ${i} >> ${RESULT_FILE1}
+            
+        echo "[Info] This Test, current Thread=${i}: Run benchmark - Mixed(Read 40%, Update 40%, Read-Modify-Write 20%) ..." >> ${RESULT_FILE1}
+        /root/ycsb-0.17.0/bin/ycsb  run mongodb -P $(dirname $0)/workload_mongo_mixed -p mongodb.url=${MONGO_URL} \
+            -threads ${i} >> ${RESULT_FILE1} 
     
         ### 完成测试后删除数据库
         # remove_database && \
@@ -73,7 +71,6 @@ EOF
 rm -rf /data/mongodb/ycsb/ && ll /data/mongodb/
 }
 
-
 ## 执行 benchmark 测试
 source /tmp/temp-setting
 RESULT_PATH="/root/ec2-test-suite/benchmark-result-files"
@@ -81,21 +78,21 @@ mkdir -p ${RESULT_PATH}
 
 submit_task ${1}
 
-### 查看结果
-# Load 结果数据：吞吐
-grep Throughput *load* ｜ awk -F"," '{print $3}'
-grep INSERT *load* | grep AverageLatency ｜ awk -F"," '{print $3}'
-grep INSERT *load* | grep 99thPercentileLatency ｜ awk -F"," '{print $3}'
-# Run 结果数据：时延
-## readonly 平均和P99时延，打印1/4/7......行(每隔3行)
-grep AverageLatency mongo_*-run.txt | grep READ  | sed -n '1~3p' | awk -F", " '{print $3}'
-grep 99thPercentileLatency mongo_*-run.txt | grep READ  | sed -n '1~3p' | awk -F", " '{print $3}'
-## updateonly 平均和P99时延，打印1/3/5......行(每隔2行)
-grep AverageLatency mongo_*-run.txt | grep UPDATE  | sed -n '1~2p' | awk -F", " '{print $3}'
-grep 99thPercentileLatency mongo_*-run.txt | grep UPDATE  | sed -n '1~2p' | awk -F", " '{print $3}'
-## read-modify-write 平均和P99时延
-grep AverageLatency mongo_*-run.txt | grep MODIFY | awk -F", " '{print $3}'
-grep 99thPercentileLatency mongo_*-run.txt | grep MODIFY | awk -F", " '{print $3}'
+# ### 查看结果
+# # Load 结果数据：吞吐
+# grep Throughput *load* ｜ awk -F"," '{print $3}'
+# grep INSERT *load* | grep AverageLatency ｜ awk -F"," '{print $3}'
+# grep INSERT *load* | grep 99thPercentileLatency ｜ awk -F"," '{print $3}'
+# # Run 结果数据：时延
+# ## readonly 平均和P99时延，打印1/4/7......行(每隔3行)
+# grep AverageLatency mongo_*-run.txt | grep READ  | sed -n '1~3p' | awk -F", " '{print $3}'
+# grep 99thPercentileLatency mongo_*-run.txt | grep READ  | sed -n '1~3p' | awk -F", " '{print $3}'
+# ## updateonly 平均和P99时延，打印1/3/5......行(每隔2行)
+# grep AverageLatency mongo_*-run.txt | grep UPDATE  | sed -n '1~2p' | awk -F", " '{print $3}'
+# grep 99thPercentileLatency mongo_*-run.txt | grep UPDATE  | sed -n '1~2p' | awk -F", " '{print $3}'
+# ## read-modify-write 平均和P99时延
+# grep AverageLatency mongo_*-run.txt | grep MODIFY | awk -F", " '{print $3}'
+# grep 99thPercentileLatency mongo_*-run.txt | grep MODIFY | awk -F", " '{print $3}'
 
 
 
--- mongo/install-sut.sh
+++ mongo/install-sut.sh
@@ -6,14 +6,14 @@ SUT_NAME=${1}
 echo "$0: Install SUT_NAME: ${SUT_NAME}"
 
 install_public_tools(){
-	yum install -y dmidecode
-	
-	## OS CONFIG
-	sysctl -w vm.max_map_count=98000
-	sysctl -w kernel.pid_max=64000
-	sysctl -w kernel.threads-max=64000
-	sysctl -w vm.max_map_count=128000
-	sysctl -w net.core.somaxconn=65535
+    yum install -y dmidecode
+    
+    ## OS CONFIG
+    sysctl -w vm.max_map_count=98000
+    sysctl -w kernel.pid_max=64000
+    sysctl -w kernel.threads-max=64000
+    sysctl -w vm.max_map_count=128000
+    sysctl -w net.core.somaxconn=65535
 }
 
 install_mongo(){
@@ -22,12 +22,12 @@ install_mongo(){
     MONGO_USER_GROUP=$(grep mongo /etc/passwd | awk -F ":" '{print $1}')
     
     mkdir -p /data/mongodb
-	chown -R ${MONGO_USER_GROUP}:${MONGO_USER_GROUP} /data/mongodb
-	cp ${MONGO_CONF} ${MONGO_CONF}.bak
+    chown -R ${MONGO_USER_GROUP}:${MONGO_USER_GROUP} /data/mongodb
+    cp ${MONGO_CONF} ${MONGO_CONF}.bak
 	
-	## 设置 cacheSizeGB
-	MEM_TOTAL_GB=$(free -g |grep Mem | awk -F " " '{print $2}')
-	let XXX=${MEM_TOTAL_GB}*80/100
+    ## 设置 cacheSizeGB
+    MEM_TOTAL_GB=$(free -g |grep Mem | awk -F " " '{print $2}')
+    let XXX=${MEM_TOTAL_GB}*80/100
 	
     cat << EOF > ${MONGO_CONF}
 systemLog:
@@ -62,26 +62,26 @@ storage:
 
 EOF
 
-	systemctl restart ${MONGO_SERVICE}
-	systemctl status ${MONGO_SERVICE}
-	sleep 5
+    systemctl restart ${MONGO_SERVICE}
+    systemctl status ${MONGO_SERVICE}
+    sleep 5
 }
 
 init_start_mongo(){
     ## 创建 root 用户
-	mongosh << EOF
+    mongosh << EOF
 use admin
 db.createUser({user:'root',pwd:'gv2mongo',roles:['root']});
 exit
 EOF
-	echo "security:" >> ${MONGO_CONF}
-	echo "  authorization: enabled" >> ${MONGO_CONF}
+    echo "security:" >> ${MONGO_CONF}
+    echo "  authorization: enabled" >> ${MONGO_CONF}
 
-	systemctl restart ${MONGO_SERVICE}
-	systemctl status ${MONGO_SERVICE}
-	
-	wget --quiet https://atlas-education.s3.amazonaws.com/sampledata.archive
-	mongorestore --archive=sampledata.archive --username root --password gv2mongo
+    systemctl restart ${MONGO_SERVICE}
+    systemctl status ${MONGO_SERVICE}
+    
+    wget --quiet https://atlas-education.s3.amazonaws.com/sampledata.archive
+    mongorestore --archive=sampledata.archive --username root --password gv2mongo
 }
 
 # 主要流程
