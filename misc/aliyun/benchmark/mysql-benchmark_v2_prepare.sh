cffb11e31646bbbd8461062005e7437ec71a4f34
eric-yq <yuanquan8261@163.com>
2025-09-25 23:16:07 +0000

    change

---
+++ misc/aliyun/benchmark/mysql-benchmark_v2_prepare.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+## 使用方法： bash mysql-benchmark_v2_prepare.sh <IP地址> <数据量(G)> <FLAG1(optional)>
+
+set -e
+
+SUT_IP_ADDR=${1}
+DATA_SIZE=${2}
+FLAG1=${3}
+
+# 其他变量
+SUT_NAME="mysql"
+OS_TYPE="alinux3"
+INSTANCE_TYPE=$(sshpass -p '3k3j9knjM' ssh -o StrictHostKeyChecking=no root@$SUT_IP_ADDR "cloud-init query ds.meta-data.instance.instance-type")
+
+let WARES=${DATA_SIZE}*1024/90
+echo "Warehouse: ${WARES}, Data size: ${DATA_SIZE}"
+
+cd ~/HammerDB-4.4/
+
+## 创建 buildschema 脚本VUSER_NUM
+cat << EOF > tpcc_buildschema.tcl
+dbset db mysql
+dbset bm TPC-C
+diset connection mysql_host ${SUT_IP_ADDR}
+diset tpcc mysql_pass gv2mysql
+diset tpcc mysql_count_ware ${WARES}
+diset tpcc mysql_num_vu 16
+buildschema
+waittocomplete
+vudestroy
+EOF
+
+## 执行 benchmark 测试，准备数据
+# source /tmp/temp-setting
+
+if [[ x"$SUT_NAME" == x ]]; then
+    SUT_NAME="mysql_hammerdb"
+fi
+
+if [[ x"$INSTANCE_TYPE" == x ]]; then
+    INSTANCE_TYPE=$FLAG1
+fi
+
+if [[ x"$INSTANCE_IP_MASTER" == x ]]; then
+    INSTANCE_IP_MASTER=$SUT_IP_ADDR
+fi
+
+RESULT_PATH="/root/benchmark-result-files"
+mkdir -p ${RESULT_PATH}
+RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}.txt"
+
+## 启动一个后台进程，执行dool命令，获取系统性能信息
+## Note: prepare: 按 38 分钟计算;  run: 按 64*8=512 分钟计算 ，总计打印 550 分钟的监控信息。
+DOOL_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}_dool.txt"
+sshpass -p '3k3j9knjM' ssh -o StrictHostKeyChecking=no root@${SUT_IP_ADDR} \
+  "dool --cpu --sys --mem --net --net-packets --disk --io --proc-count --time --bits 60 550" \
+  1> ${DOOL_FILE} 2>&1 &
+
+echo "Test Detail on $(date)====================================================================================" >> ${RESULT_FILE}
+## 远程输出 /etc/my.cnf 内容
+echo "Export MySQL configuration /etc/my.cnf first... " >> ${RESULT_FILE}
+sshpass -p '3k3j9knjM' ssh -o StrictHostKeyChecking=no root@${SUT_IP_ADDR} "sudo cat /etc/my.cnf" >> ${RESULT_FILE}
+
+## 准备数据
+echo "Start to prepare data. SUT_IP_ADDR=${SUT_IP_ADDR}, Data size: ${DATA_SIZE}, Warehouse: ${WARES},, FLAG1=${5}" >> ${RESULT_FILE}
+mysql -h ${SUT_IP_ADDR} -p'gv2mysql' -e "drop database tpcc;"
+./hammerdbcli auto tpcc_buildschema.tcl
+
+sleep 10
+
+## 更新 schema_information
+mysql -h ${SUT_IP_ADDR} -p'gv2mysql' << EOF
+analyze table tpcc.stock;
+analyze table tpcc.order_line;
+analyze table tpcc.customer;
+analyze table tpcc.history;
+analyze table tpcc.orders;
+analyze table tpcc.new_order;
+analyze table tpcc.item;
+analyze table tpcc.district;
+analyze table tpcc.warehouse;
+EOF
+
+database_statics=$(mysql -h $SUT_IP_ADDR -p'gv2mysql' -e "select table_schema as '数据库', sum(table_rows) as '记录数', sum(truncate(data_length/1024/1024, 2)) as '数据容量(MB)', sum(truncate(index_length/1024/1024, 2)) as '索引容量(MB)' from information_schema.tables where table_schema='tpcc';")
+table_statics=$(mysql -h $SUT_IP_ADDR -p'gv2mysql' -e "select table_schema as '数据库', table_name as '表名', table_rows as '记录数', truncate(data_length/1024/1024, 2) as '数据容量(MB)', truncate(index_length/1024/1024, 2) as '索引容量(MB)' from information_schema.tables where table_schema='tpcc' order by data_length desc, index_length desc;")
+
+echo "[Build Schema Summary]: " >> ${RESULT_FILE}
+echo "$database_statics" >> ${RESULT_FILE}
+echo "$table_statics" >> ${RESULT_FILE}
+
+echo "Complete to prepare data. " >> ${RESULT_FILE}
--- misc/aliyun/submit-benchmark-tasks.sh
+++ misc/aliyun/submit-benchmark-tasks.sh
@@ -24,3 +24,25 @@ sleep 60
 bash benchmark/valkey-benchmark_v1.sh ${IPADDR} 8007 180
 echo "[Info] Valkey on $IPADDR complete."
 echo "[Info] sleep 60 seconds for next benchmark..."
+
+
+##########################################################################################
+## mysql
+IPADDR_R8Y="172.24.133.63"
+IPADDR_R9I="172.24.133.XX"
+IPADDR_R9A="172.24.133.xx"
+IPADDR_R9AE="172.24.133.xx"
+## 选择测试的 IP 地址
+INSTANCE_IP_MASTER=$IPADDR_R8Y
+## 准备数据
+bash benchmark/mysql-benchmark_v2_prepare.sh ${INSTANCE_IP_MASTER} 64
+
+## 使用不同的vuser执行benchmark
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 60  1 64 
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 60  2 64
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 60  4 64 
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 60  6 64  
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 60  8 64 
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 30 10 64 
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 30 12 64
+bash benchmark/mysql-benchmark_v2_run.sh ${INSTANCE_IP_MASTER} 30 16 64
