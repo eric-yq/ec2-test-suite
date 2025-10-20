b8b74dd2b1cf000f6ba30149a68e94993d7bf8d7
eric-yq <yuanquan8261@163.com>
2025-10-20 11:49:42 +0000

    change

--- benchmark/petclinic-benchmark.sh
+++ benchmark/petclinic-benchmark.sh
@@ -26,5 +26,5 @@ jmeter -n -t $(dirname $0)/petclinic_test_plan.jmx \
 echo "[Info] Complete jmeter test for USERS=$i. "
 #解析结果
 REPORT_DIR="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${SUT_IP_ADDR}_${SUT_PORT}_${i}_report"
-jmeter -g ${RESULT_FILE} -o ./${REPORT_DIR}
+jmeter -g ${RESULT_FILE} -o ${REPORT_DIR}
 # rm -rf ${RESULT_FILE}
