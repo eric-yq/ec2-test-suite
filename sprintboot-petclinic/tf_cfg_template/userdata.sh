2888f10dabb0f72d17b1834f4aaa7d9713eb8996
eric-yq <yuanquan8261@163.com>
2025-10-21 09:50:01 +0000

    change

--- sprintboot-petclinic/tf_cfg_template/userdata.sh
+++ sprintboot-petclinic/tf_cfg_template/userdata.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
 
+set -e
+
+# 实例启动成功之后的首次启动 OS， /root/userdata.sh 不存在，创建该 userdata.sh 文件并设置开启自动执行该脚本。
+if [ ! -f "/root/userdata.sh" ]; then
+    echo "首次启动 OS, 未找到 /root/userdata.sh，准备创建..."
+    # 复制文件
+    cp /var/lib/cloud/instance/scripts/part-001 /root/userdata.sh
+    chmod +x /root/userdata.sh
+    # 创建 systemd 服务单元
+    cat > /etc/systemd/system/userdata.service << EOF
+[Unit]
+Description=Execute userdata script at boot
+After=network.target
+
+[Service]
+Type=oneshot
+User=root
+ExecStart=/root/userdata.sh
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    # 启用服务
+    systemctl daemon-reload
+    systemctl enable userdata.service
+    
+    echo "已创建并启用 systemd 服务 userdata.service"
+    exit 0
+fi
+
+
 ## 安装 Java
 yum update -y
 
 ## Corretto 25
 yum install -y java-25-amazon-corretto java-25-amazon-corretto-devel python3-pip git
 pip3 install dool
-JDK_VERSION='corretto25'
-java -version
 
 ## 启动 spring-petclinic 应用
 cd /root/
 git clone https://github.com/spring-projects/spring-petclinic.git
 cd spring-petclinic
-echo "yuanquan1: HOME is $HOME"
 export HOME=/root/
-echo "yuanquan2: HOME is $HOME"
 ./mvnw package
 cp target/*.jar /usr/local/bin/spring-petclinic.jar
-#nohup java -jar target/*.jar &
 
 #创建 systemd 服务文件:
 cat << EOF > /etc/systemd/system/petclinic.service
@@ -41,6 +68,3 @@ EOF
 systemctl daemon-reload
 systemctl enable petclinic.service
 systemctl start petclinic.service
-# 检查状态:
-systemctl status petclinic.service
-
