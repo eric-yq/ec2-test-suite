e713c62d1ace09dd63911cb23e317e93a39a82e3
eric-yq <yuanquan8261@163.com>
2025-10-21 06:52:27 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-petclinic.sh
+++ samples-of-submit-benchmark/submit-benchmark-petclinic.sh
@@ -23,8 +23,8 @@ do
 			continue
 		fi
 
-		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 180 seconds ..."
-		sleep 180
+		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 300 seconds ..."
+		sleep 300
 
 		## 执行 Benchmark 测试
 		echo "$0: Star to run benchmark"
