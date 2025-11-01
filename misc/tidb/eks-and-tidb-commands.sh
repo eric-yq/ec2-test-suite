40fbb7831d9a2822dae68be10cebdab0859f78e7
eric-yq <yuanquan8261@163.com>
2025-11-01 12:48:48 +0000

    change

--- misc/tidb/eks-and-tidb-commands.sh
+++ misc/tidb/eks-and-tidb-commands.sh
@@ -25,15 +25,21 @@ tidb_port=4000
 tidb_host="a8a4039336bd04644a7bfe24b4b39286-74e2214757238f41.elb.us-east-2.amazonaws.com"
 tidb_port=4000
 
+# -- 查询tiflash 同步状态
+mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e \
+  "SELECT * FROM information_schema.tiflash_replica WHERE TABLE_SCHEMA = 'tpch300' ORDER BY TABLE_NAME;"
+
 # -- 查询所有存储节点信息
-mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e "SELECT store_id, address, store_state, capacity, available, capacity - available AS used, (capacity - available) / capacity AS usage_ratio, version FROM information_schema.tikv_store_status ORDER BY address;"
+mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e \
+  "SELECT store_id, address, store_state, capacity, available, capacity - available AS used, (capacity - available) / capacity AS usage_ratio, version FROM information_schema.tikv_store_status ORDER BY address;"
  
 ## -- 查询各个表占用的磁盘容量
-mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e "SELECT table_name, CONCAT(ROUND(data_length/1024/1024, 2), ' MB') as data_size FROM information_schema.tables WHERE table_schema = 'tpch300' ORDER BY data_length DESC;"
+mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e \
+  "SELECT table_name, CONCAT(ROUND(data_length/1024/1024, 2), ' MB') as data_size FROM information_schema.tables WHERE table_schema = 'tpch300' ORDER BY data_length DESC;"
 
 ## -- 查询各个表的记录数
-mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e "SELECT table_name, table_rows FROM information_schema.tables  WHERE table_schema = 'tpch300' ORDER BY table_name;"
+mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e \
+  "SELECT table_name, table_rows FROM information_schema.tables  WHERE table_schema = 'tpch300' ORDER BY table_name;"
 
-mysql --comments  -h ${tidb_host} -P ${tidb_port} -u root -e "SELECT * FROM information_schema.tiflash_replica WHERE TABLE_SCHEMA = 'tpch300' ORDER BY TABLE_NAME;"
 
 #################################################################################################################
