28c7f8d0da0ba36deb2b01181a183a14ef7e2f15
eric-yq <yuanquan8261@163.com>
2025-11-07 13:33:28 +0000

    change

--- ffmpeg/tf_cfg_template/userdata.sh
+++ ffmpeg/tf_cfg_template/userdata.sh
@@ -157,3 +157,8 @@ ffmpeg -hide_banner -codecs > ${archive}/filesize.txt
 du -smh *.mp4 > ${archive}/filesize.txt
 tar czf ${archive}.tar.gz ${archive}/
 aws s3 cp ${archive}.tar.gz s3://ec2-core-benchmark-ericyq/result_ffmpeg/
+
+# 停止实例
+INSTANCE_ID=$(ec2-metadata --quiet --instance-id)
+REGION_ID=$(ec2-metadata --quiet --region)
+aws ec2 stop-instances --instance-ids ${INSTANCE_ID} --region ${REGION_ID}
--- specjbb15/tf_cfg_template/main.tf
+++ specjbb15/tf_cfg_template/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "sut_server" {
   ami 						= var.ami_id
   subnet_id					= var.subnet_id
   vpc_security_group_ids 	= var.vpc_security_group_ids
-  placement_group           = var.placement_group_name
+  # placement_group           = var.placement_group_name
   key_name 					= var.ami_key_pair_name
   user_data 				= file(var.userdata_file)
 
