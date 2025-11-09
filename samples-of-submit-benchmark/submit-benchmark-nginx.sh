2b8aa323ada9c3cd234f2947c8fea123cf5f6f05
eric-yq <yuanquan8261@163.com>
2025-11-09 04:22:41 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-nginx.sh
+++ samples-of-submit-benchmark/submit-benchmark-nginx.sh
@@ -2,8 +2,8 @@
 
 # 待测 EC2 规格和 OS
 os_types="al2023"
-instance_types="r8g.2xlarge r8i.2xlarge r8a.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
-
+# instance_types="r8g.2xlarge r8i.2xlarge r8a.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
+instance_types=$1
 
 for os in ${os_types} 
 do
@@ -16,6 +16,8 @@ do
 		launch_status=$?
 		if [ $launch_status -ne 0 ]; then
 			echo "\$0: [$(date +%Y%m%d.%H%M%S)] Instance launch failed for OS_TYPE=${os}, INSTANCE_TYPE=${ins}. Continuing with next configuration..."
+			rm -rf nginx-webserver/tf_cfg_nginx-webserver/
+			rm -rf nginx-loadbalancer/tf_cfg_nginx-loadbalance/
 			continue
 		fi
 
