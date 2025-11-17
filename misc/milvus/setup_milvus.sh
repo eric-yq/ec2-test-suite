9ba794b3c06e677ec6b19a5afdfd3366484b2e53
eric-yq <yuanquan8261@163.com>
2025-11-17 10:06:49 +0000

    change

--- misc/milvus/setup_milvus.sh
+++ misc/milvus/setup_milvus.sh
@@ -21,7 +21,38 @@ docker-compose up -d
 ## 查看状态
 docker-compose ps
 
-
 #（可选）安装 Web 管理工具
 docker run -d -p 8000:3000 -e MILVUS_URL=$(hostname -i):19530 zilliz/attu:v2.4
 
+
+
+###############################################################################################
+# 安装 vdbbench, m6i.2xlarge + AL2023
+
+sudo su - root
+
+## Need isntall Python 3.11 or above
+dnf install -y python3.11 python3.11-pip python3.11-devel git gcc gcc-c++
+python3.11 -V
+
+## Install VectorDBBench
+pip3.11 install vectordb-bench ujson
+which init_bench
+
+## Redirect /tmp/vectordb_bench to another location
+mkdir -p /root/vectordb_bench
+ln -s /root/vectordb_bench /tmp/vectordb_bench
+
+## init benchmark tool，follow the prompt to enter web page
+# init_bench
+
+## 通过命令行进行测试
+# screen -R ttt -L
+instance_type="r8i.2xlarge"
+ipaddr="172.31.11.93"
+vectordbbench milvushnsw \
+  --case-type Performance768D1M \
+  --m 30 --ef-construction 360 --ef-search 100 \
+  --task-label milvus-$instance_type \
+  --uri http://$ipaddr:19530 
+  
