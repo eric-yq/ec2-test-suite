14563eb2bedd37238d2831568afa53cb158a0e50
eric-yq <yuanquan8261@163.com>
2025-12-17 10:29:46 +0000

    change

--- misc/oceanbase/tpch/load.py
+++ misc/oceanbase/tpch/load.py
@@ -6,10 +6,10 @@ import time
 import subprocess
 
 hostname='127.0.0.1'  # OceanBase数据库链接地址
-port='2883'               #端口号
+port='2881'               #端口号
 user='root'               # 用户名
 tenant='sys'
-password='GH7GM1wVfjUiMSmrpyFy'           # 密码
+password='xxxxxxxxx'           # 密码
 data_path='/root/TPC-H_Tools_v3.0.0/dbgen/tpch50'         # 注意！！请填写压力机ECS下 tbl 所在目录
 db_name='oceanbase'             # 数据库名
 
