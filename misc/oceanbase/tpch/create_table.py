a2fe8181e131db7080564acb6bc1661b90fc4238
eric-yq <yuanquan8261@163.com>
2025-12-17 10:29:46 +0000

    change

--- misc/oceanbase/tpch/create_table.py
+++ misc/oceanbase/tpch/create_table.py
@@ -6,12 +6,12 @@ import time
 import subprocess
 
 hostname='127.0.0.1'  # OceanBase数据库链接地址
-port='2883'               #端口号
-user='root'               # 用户名
+port='2881'           #端口号
+user='root'           # 用户名
 tenant='sys'
-password='GH7GM1wVfjUiMSmrpyFy'           # 密码
-data_path='/root/TPC-H_Tools_v3.0.0/dbgen/tpch50'         # 注意！！请填写压力机ECS下 tbl 所在目录
-db_name='oceanbase'             # 数据库名
+password='xxxxxxxxx'  # 密码
+data_path='/root/TPC-H_Tools_v3.0.0/dbgen/tpch50' # 注意！！请填写压力机ECS下 tbl 所在目录
+db_name='oceanbase'   # 数据库名
 
 # 创建表
 cmd_str='obclient -h%s -P%s -u%s@%s -p%s -D%s < create_tpch_mysql_table_part.ddl'%(hostname,port,user,tenant,password,db_name)
