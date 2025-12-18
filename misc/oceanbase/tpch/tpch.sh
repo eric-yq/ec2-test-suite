7fcf629ebd35b6dac29b1e415feb3552b7fa0a1f
eric-yq <yuanquan8261@163.com>
2025-12-18 00:44:14 +0000

    change

--- misc/oceanbase/tpch/tpch.sh
+++ misc/oceanbase/tpch/tpch.sh
@@ -20,3 +20,10 @@ do
     costtime=`echo $stoptime $starttime | awk '{printf "%0.2f\n", ($1 - $2) / 1000000000}'`
     echo `date  '+[%Y-%m-%d %H:%M:%S]'` "END,COST ${costtime}s"
 done
+
+## 单独执行某条SQL
+# PASSWORD="GH7GM1wVfjUiMSmrpyFy"
+# TPCH_TEST="obclient -h 127.0.0.1 -P 2881 -uroot@sys -D oceanbase -p${PASSWORD} -c" 
+# sql="source db1.sql"
+# echo $sql| $TPCH_TEST
+
