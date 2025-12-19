8110837fdaf9508a901467af676580660e09b79a
eric-yq <yuanquan8261@163.com>
2025-12-19 11:11:36 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-mongo-test_v2.sh
+++ samples-of-submit-benchmark/submit-benchmark-mongo-test_v2.sh
@@ -19,8 +19,8 @@ do
 			continue
 		fi
 		
-		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 300 seconds..."
-		sleep 300
+		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 600 seconds..."
+		sleep 600
 		
 		## 执行 Benchmark 测试
 		echo "$0: Star to run benchmark"
