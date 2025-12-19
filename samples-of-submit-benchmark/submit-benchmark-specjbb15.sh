2c5b948b84e0b37a848b8570d761660990dfc875
eric-yq <yuanquan8261@163.com>
2025-12-19 02:25:06 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-specjbb15.sh
+++ samples-of-submit-benchmark/submit-benchmark-specjbb15.sh
@@ -2,8 +2,8 @@
 
 # 待测 EC2 规格和 OS
 os_types="al2023"
-# instance_types="r8a.2xlarge r8g.2xlarge r8i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
-instance_types="m8a.2xlarge m8g.2xlarge m8i.2xlarge m7a.2xlarge m7g.2xlarge m7i.2xlarge m6a.2xlarge m6g.2xlarge m6i.2xlarge"
+# instance_types="r8a.4xlarge r8g.4xlarge r8i.4xlarge r7a.4xlarge r7g.4xlarge r7i.4xlarge r6a.4xlarge r6g.4xlarge r6i.4xlarge"
+instance_types="m8a.4xlarge m8g.4xlarge m8i.4xlarge m7a.4xlarge m7g.4xlarge m7i.4xlarge m6a.4xlarge m6g.4xlarge m6i.4xlarge"
 
 for os in ${os_types} 
 do
