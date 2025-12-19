1930f1317ad5cfd90510283ed213e733763d2381
eric-yq <yuanquan8261@163.com>
2025-12-19 00:49:35 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-nginx.sh
+++ samples-of-submit-benchmark/submit-benchmark-nginx.sh
@@ -2,8 +2,9 @@
 
 # 待测 EC2 规格和 OS
 os_types="al2023"
-# instance_types="r8g.2xlarge r8i.2xlarge r8a.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
-instance_types=$1
+# instance_types=$1
+# instance_types="r8a.2xlarge r8g.2xlarge r8i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
+instance_types="m8a.2xlarge m8g.2xlarge m8i.2xlarge m7a.2xlarge m7g.2xlarge m7i.2xlarge m6a.2xlarge m6g.2xlarge m6i.2xlarge"
 
 for os in ${os_types} 
 do
@@ -15,7 +16,7 @@ do
 		# 检查实例启动状态：如果失败则跳过后续测试。
 		launch_status=$?
 		if [ $launch_status -ne 0 ]; then
-			echo "\$0: [$(date +%Y%m%d.%H%M%S)] Instance launch failed for OS_TYPE=${os}, INSTANCE_TYPE=${ins}. Continuing with next configuration..."
+			echo "$0: [$(date +%Y%m%d.%H%M%S)] Instance launch failed for OS_TYPE=${os}, INSTANCE_TYPE=${ins}. Continuing with next configuration..."
 			rm -rf nginx-webserver/tf_cfg_nginx-webserver/
 			rm -rf nginx-loadbalancer/tf_cfg_nginx-loadbalance/
 			continue
