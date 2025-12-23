8cfe2791f73623c5b348e6df48f9865151789528
eric-yq <yuanquan8261@163.com>
2025-12-23 11:51:41 +0000

    change

--- samples-of-submit-benchmark/submit-all-in-one.sh
+++ samples-of-submit-benchmark/submit-all-in-one.sh
@@ -52,6 +52,37 @@ do
     done
 done
 
+sleep 60
+## Nginx
+for os in ${os_types} 
+do
+    for ins in ${instance_types} 
+        do
+        ## 创建实例、安装软件
+        echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
+        bash launch-instances-nginx.sh -s nginx -t ${ins} -o ${os}
+        # 检查实例启动状态：如果失败则跳过后续测试。
+        launch_status=$?
+        if [ $launch_status -ne 0 ]; then
+            echo "$0: [$(date +%Y%m%d.%H%M%S)] Instance launch failed for OS_TYPE=${os}, INSTANCE_TYPE=${ins}. Continuing with next configuration..."
+            rm -rf nginx-webserver/tf_cfg_nginx-webserver/
+            rm -rf nginx-loadbalancer/tf_cfg_nginx-loadbalance/
+            continue
+        fi
+
+        echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 600 seconds..."
+        sleep 600
+
+        ## 执行 Benchmark 测试
+        echo "$0: Star to run benchmark"
+        source /tmp/temp-setting
+        bash benchmark/nginx-benchmark.sh ${INSTANCE_IP_LOADBALANCE}
+
+        ## 停止实例
+        aws ec2 terminate-instances --region $(cloud-init query region) \
+        --instance-ids ${INSTANCE_ID_LOADBALANCE} ${INSTANCE_ID_WEB1} ${INSTANCE_ID_WEB2} &
+    done
+done
 
 sleep 60
 ## Redis
@@ -116,38 +147,6 @@ do
 
         ## 停止实例
         aws ec2 terminate-instances --instance-ids ${INSTANCE_ID} --region $(cloud-init query region) &
-    done
-done
-
-sleep 60
-## Nginx
-for os in ${os_types} 
-do
-    for ins in ${instance_types} 
-        do
-        ## 创建实例、安装软件
-        echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
-        bash launch-instances-nginx.sh -s nginx -t ${ins} -o ${os}
-        # 检查实例启动状态：如果失败则跳过后续测试。
-        launch_status=$?
-        if [ $launch_status -ne 0 ]; then
-            echo "$0: [$(date +%Y%m%d.%H%M%S)] Instance launch failed for OS_TYPE=${os}, INSTANCE_TYPE=${ins}. Continuing with next configuration..."
-            rm -rf nginx-webserver/tf_cfg_nginx-webserver/
-            rm -rf nginx-loadbalancer/tf_cfg_nginx-loadbalance/
-            continue
-        fi
-
-        echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 600 seconds..."
-        sleep 600
-
-        ## 执行 Benchmark 测试
-        echo "$0: Star to run benchmark"
-        source /tmp/temp-setting
-        bash benchmark/nginx-benchmark.sh ${INSTANCE_IP_LOADBALANCE}
-
-        ## 停止实例
-        aws ec2 terminate-instances --region $(cloud-init query region) \
-        --instance-ids ${INSTANCE_ID_LOADBALANCE} ${INSTANCE_ID_WEB1} ${INSTANCE_ID_WEB2} &
     done
 done
 
