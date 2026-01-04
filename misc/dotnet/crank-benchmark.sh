63e7aac70b3ca0a5bb0fdb7f212d01a86990afab
eric-yq <yuanquan8261@163.com>
2026-01-04 04:14:52 +0000

    change

--- misc/dotnet/crank-benchmark.sh
+++ misc/dotnet/crank-benchmark.sh
@@ -2,9 +2,14 @@
 
 # Amazon Linux 2023
 
+# 组网说明：
+# 1. 在 Load Generator 上安装 crank 和 crank-agent，SUT 上只安装 crank-agent.
+# 2. 在 Load Generator 上运行 crank 命令发起测试，crank-agent 负责在 Load Generator 和 SUT 上启动应用和负载生成器。
+
 sudo su - root
 
-yum install -y git aspnetcore-runtime-8.0 dotnet-runtime-8.0 dotnet-sdk-8.0 
+yum install -y git aspnetcore-runtime-8.0 dotnet-runtime-8.0 dotnet-sdk-8.0 python3-pip
+pip3 install dool
 
 ## 在 Load Generator 和 SUT 上都安装 Crank Controller 和 Agent
 dotnet tool install -g Microsoft.Crank.Controller --version "0.2.0-*" 
@@ -27,9 +32,6 @@ cp /root/Benchmarks/src/BenchmarksApps/Mvc/benchmarks.crudapi.yml \
    /root/Benchmarks/src/BenchmarksApps/Mvc/benchmarks.crudapi.yml.bak
 cd /root/Benchmarks/src/BenchmarksApps/Mvc/
 
-SUT_IPADDR="172.31.4.109"
-SUT_INSTANCE="c8i.xlarge"
-
 cat << EOF >> benchmarks.crudapi.yml
 
 profiles:
@@ -43,16 +45,20 @@ profiles:
         endpoints: "http://default-load:5010"
 EOF
 
-SUT_IPADDR="172.31.10.199"
-SUT_INSTANCE="c7g.xlarge"
+## 测试方式 1(Local)：Application 和 Load 在同一台机器上运行，
+SUT_IPADDR="172.31.91.179"
+SUT_INSTANCE="m7g.2xlarge"
 
-SUT_IPADDR="172.31.15.192"
-SUT_INSTANCE="c8g.xlarge"
+SUT_IPADDR="172.31.88.38"
+SUT_INSTANCE="m8g.2xlarge"
 
-SUT_IPADDR="172.31.4.109"
-SUT_INSTANCE="c8i.xlarge"
+SUT_IPADDR="172.31.91.116"
+SUT_INSTANCE="m9g-preview.2xlarge"
 
-## 测试方式 1：Application 和 Load 在同一台机器上运行
+SUT_IPADDR="172.31.90.44"
+SUT_INSTANCE="m8i.2xlarge"
+
+let XXX=$(nproc)
 crank --config ./benchmarks.crudapi.yml \
       --scenario ApiCrudListProducts \
       --profile my-profile \
@@ -61,10 +67,26 @@ crank --config ./benchmarks.crudapi.yml \
       --load.endpoints http://$SUT_IPADDR:5010 \
       --variable serverAddress=$SUT_IPADDR \
       --variable duration=60 \
-      --variable connections=4 \
+      --variable connections=$XXX \
       --json results-local-crudapi-$SUT_INSTANCE-$SUT_IPADDR.json
 
-## 测试方式 2：Application 和 Load 在两台不同的机器上运行
+#### 比较结果
+crank compare results-local-crudapi-*.json
+
+## 测试方式 2(Remote)：Application 和 Load 在两台不同的机器上运行
+SUT_IPADDR="172.31.91.179"
+SUT_INSTANCE="m7g.2xlarge"
+
+SUT_IPADDR="172.31.88.38"
+SUT_INSTANCE="m8g.2xlarge"
+
+SUT_IPADDR="172.31.91.116"
+SUT_INSTANCE="m9g-preview.2xlarge"
+
+SUT_IPADDR="172.31.90.44"
+SUT_INSTANCE="m8i.2xlarge"
+
+let XXX=$(nproc)
 crank --config ./benchmarks.crudapi.yml \
       --scenario ApiCrudListProducts \
       --profile my-profile \
@@ -73,8 +95,10 @@ crank --config ./benchmarks.crudapi.yml \
       --load.endpoints http://localhost:5010 \
       --variable serverAddress=$SUT_IPADDR \
       --variable duration=60 \
-      --variable connections=4 \
+      --variable connections=$XXX \
       --json results-remote-crudapi-$SUT_INSTANCE-$SUT_IPADDR.json
 
-## 比较多个结果
-crank compare results-crudapi-*.json
+#### 比较结果
+crank compare results-remote-crudapi-*.json
+
+
