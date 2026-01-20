45c9a34166c41d79732a4114ebd666274901ec65
eric-yq <yuanquan8261@163.com>
2026-01-20 11:40:14 +0000

    change

--- benchmark/mysql-benchmark_sysbench_run-jd.sh
+++ benchmark/mysql-benchmark_sysbench_run-jd.sh
@@ -9,6 +9,7 @@ OLTP_DURATION=${2}
 TABLES=${3}
 TABLE_SIZE=${4}
 RUN_THREADS=${5}
+PROFILE_MODE=${6}
 
 cd ~/sysbench-1.0.20/
 
@@ -18,7 +19,48 @@ mkdir -p ${RESULT_PATH}
 RESULT_FILE="${RESULT_PATH}/${SUT_NAME}_${INSTANCE_TYPE}_${OS_TYPE}_${INSTANCE_IP_MASTER}.txt"
 
 echo "Test Detail on $(date)====================================================================================" >> ${RESULT_FILE}
-echo "Command Line Parameters: SUT_IP_ADDR=${1}, OLTP_DURATION=${2}, TABLES=${3}, TABLE_SIZE=${4}, RUN_THREADS=${5}" >> ${RESULT_FILE}
+echo "Command Line Parameters: SUT_IP_ADDR=${1}, OLTP_DURATION=${2}, TABLES=${3}, TABLE_SIZE=${4}, RUN_THREADS=${5}, PROFILE_MODE=${6}" >> ${RESULT_FILE}
+
+read_only, oltp_read_only,
+write_only, oltp_write_only, 
+rw_default, oltp_read_write,
+rw_70_30, oltp_read_only, "--point-selects=10 --range-selects=off"
+rw_90_10,
+point_select, oltp_point_select,
+update_index, oltp_update_index,
+update_non_index, oltp_update_non_index,
+
+
+if [ "$PROFILE_MODE" = "read_only" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_read_only.lua"
+
+elif [ "$PROFILE_MODE" = "write_only" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_write_only.lua"
+
+elif [ "$PROFILE_MODE" = "rw_default" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_read_write.lua"
+
+elif [ "$PROFILE_MODE" = "rw_70_30" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_read_write.lua --point-selects=10 --range-selects=off"
+
+elif [ "$PROFILE_MODE" = "rw_90_10" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_read_write.lua --point-selects=9 --range-selects=1"
+
+elif [ "$PROFILE_MODE" = "point_select" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_point_select.lua"
+
+elif [ "$PROFILE_MODE" = "update_index" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_update_index.lua"
+
+elif [ "$PROFILE_MODE" = "update_non_index" ]; then
+  BENCHMARK_LUA="./src/lua/oltp_update_non_index.lua"
+  
+else
+  echo "Unsupported PROFILE_MODE: $PROFILE_MODE"
+  exit 1
+fi
+
+
 
 ## 执行 benchmark
 RAMPUP_DURATION=0
