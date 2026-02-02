5ac6be545e9450baa70b55692ce8ba6889a28b8f
eric-yq <yuanquan8261@163.com>
2026-02-02 07:52:24 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-blank-test.sh
+++ samples-of-submit-benchmark/submit-benchmark-blank-test.sh
@@ -27,15 +27,23 @@ do
 			continue
 		fi
 
+		# 实例类型和 IP 地址记录到文件
 		source /tmp/temp-setting
-		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 30 seconds ..."
-		sleep 30
-
-		# 执行 ping 测试
-		PING_RESULT=$(ping -q -c 30 ${INSTANCE_IP_MASTER} | tail -n 1)
-		echo "[$(date +%Y%m%d.%H%M%S)] Instance Type: ${ins}, IP: ${INSTANCE_IP_MASTER}: ${PING_RESULT}" >> /tmp/ping_latency_log.txt
-
+		echo "${ins} ${INSTANCE_IP_MASTER}" >> /tmp/servers.txt
+		
 		## 停止实例
 		# aws ec2 terminate-instances --instance-ids ${INSTANCE_ID} --region $(cloud-init query region) &
 	done
 done
+
+echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 30 seconds ..."
+sleep 30
+
+# 执行 ping 测试
+echo "[$(date +%Y%m%d.%H%M%S)] Ping latency test ..." >> /tmp/ping_latency_log.txt
+while read -r instance_type ip_address; do
+do
+  ping_result=$(ping -q -c 30 $ip_address | tail -n 1)
+  echo "[$(date +%Y%m%d.%H%M%S)]   Instance Type: ${instance_type}, IP: $ip_address: ${ping_result}" >> /tmp/ping_latency_log.txt
+done < /tmp/servers.txt
+rm -f /tmp/servers.txt
