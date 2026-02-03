866bbf0f759a47f22165eca28e29b3286df90482
eric-yq <yuanquan8261@163.com>
2026-02-03 03:24:02 +0000

    change

--- samples-of-submit-benchmark/submit-benchmark-milvus.sh
+++ samples-of-submit-benchmark/submit-benchmark-milvus.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# Redis Benchmark
 # 待测 EC2 规格和 OS
 os_types="al2023"
 instance_types="$1"
-# instance_types="r8a.2xlarge r8g.2xlarge r8i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
-# instance_types="m8a.2xlarge m8g.2xlarge m8i.2xlarge m7a.2xlarge m7g.2xlarge m7i.2xlarge m6a.2xlarge m6g.2xlarge m6i.2xlarge"
 
+## CPG 选项
 if [ "$USE_CPG" = "1" ] ; then
   OPT="USE_CPG=1"
 else
-  OPT=""
+  OPT="USE_CPG=0"
 fi
+
+# 清理临时文件
+rm -rf /tmp/servers.txt /tmp/ping_latency_log.txt
 
 for os in ${os_types} 
 do
@@ -26,13 +27,22 @@ do
 			echo "\$0: [$(date +%Y%m%d.%H%M%S)] Instance launch failed for OS_TYPE=${os}, INSTANCE_TYPE=${ins}. Continuing with next configuration..."
 			continue
 		fi
-		
-		echo "$0: [$(date +%Y%m%d.%H%M%S)] Sleep 180 seconds..."
-		sleep 180
-		
+
+		####################################
+		# 实例类型、IP 地址和实例 ID 记录到文件
+		source /tmp/temp-setting
+		echo "${ins} ${INSTANCE_IP_MASTER} ${INSTANCE_ID}" >> /tmp/servers.txt
+		echo "[$(date +%Y%m%d.%H%M%S)] Sleep 30 seconds ..." && sleep 30
+		# 执行 ping 测试
+		echo "[$(date +%Y%m%d.%H%M%S)] Ping latency test, result shows the avg. latency only. Extra option : ${OPT}" \
+  			>> /tmp/ping_latency_log.txt
+		ping_result=$(ping -q -c 60 ${INSTANCE_IP_MASTER} | tail -n 1 | awk -F '/' '{print $5 " ms"}') 
+		echo "[$(date +%Y%m%d.%H%M%S)]   ${ins}, ${INSTANCE_IP_MASTER} : ${ping_result}" \
+			>> /tmp/ping_latency_log.txt
+	    ####################################
+
 		## 执行 Benchmark 测试
 		echo "$0: Star to run benchmark"
-		source /tmp/temp-setting
 		bash benchmark/milvus-benchmark.sh ${INSTANCE_IP_MASTER}
 		
 		# 停止 dool 监控
