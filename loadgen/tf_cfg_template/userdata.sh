a338763545875071db938aeebd363876b499cbaa
eric-yq <yuanquan8261@163.com>
2026-03-10 06:54:10 +0000

    change

--- loadgen/tf_cfg_template/userdata.sh
+++ loadgen/tf_cfg_template/userdata.sh
@@ -5,37 +5,37 @@ sudo systemctl stop amazon-ssm-agent
 sudo systemctl disable amazon-ssm-agent
 
 # 实例启动成功之后的首次启动 OS， /root/userdata.sh 不存在，创建该 userdata.sh 文件并设置开启自动执行该脚本。
-if [ ! -f "/root/userdata.sh" ]; then
-    echo "首次启动 OS, 未找到 /root/userdata.sh，准备创建..."
-    # 复制文件
-    cp /var/lib/cloud/instance/scripts/part-001 /root/userdata.sh
-    chmod +x /root/userdata.sh
-    # 创建 systemd 服务单元
-    cat > /etc/systemd/system/userdata.service << EOF
-[Unit]
-Description=Execute userdata script at boot
-After=network.target
+# if [ ! -f "/root/userdata.sh" ]; then
+#     echo "首次启动 OS, 未找到 /root/userdata.sh，准备创建..."
+#     # 复制文件
+#     cp /var/lib/cloud/instance/scripts/part-001 /root/userdata.sh
+#     chmod +x /root/userdata.sh
+#     # 创建 systemd 服务单元
+#     cat > /etc/systemd/system/userdata.service << EOF
+# [Unit]
+# Description=Execute userdata script at boot
+# After=network.target
 
-[Service]
-Type=oneshot
-User=root
-ExecStart=/root/userdata.sh
-RemainAfterExit=yes
+# [Service]
+# Type=oneshot
+# User=root
+# ExecStart=/root/userdata.sh
+# RemainAfterExit=yes
 
-[Install]
-WantedBy=multi-user.target
-EOF
-    # 启用服务
-    systemctl daemon-reload
-    systemctl enable userdata.service
+# [Install]
+# WantedBy=multi-user.target
+# EOF
+#     # 启用服务
+#     systemctl daemon-reload
+#     systemctl enable userdata.service
     
-    echo "已创建并启用 systemd 服务 userdata.service"
+#     echo "已创建并启用 systemd 服务 userdata.service"
 
-    ### 等待 60 秒再执行 userdata 脚本
-    sleep 60
-    systemctl start userdata.service
-    exit 0
-fi
+#     ### 等待 60 秒再执行 userdata 脚本
+#     sleep 60
+#     systemctl start userdata.service
+#     exit 0
+# fi
 
 SUT_NAME="SUT_XXX"
 
