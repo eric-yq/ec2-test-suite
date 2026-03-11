0b6f39e22ff8d08e94f9949dfe422cf9860822bc
eric-yq <yuanquan8261@163.com>
2026-03-11 06:09:33 +0000

    change

--- submit-benchmark-singles.sh
+++ submit-benchmark-singles.sh
@@ -2,7 +2,9 @@
 
 # 待测 EC2 规格和 OS
 os_types="al2023"
-instance_types="$1"
+sut_name="$1"  ## specjbb15, ffmpeg, spark, pts
+instance_types="$2"
+
 # instance_types="r8a.2xlarge r8g.2xlarge r8i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
 # instance_types="m8a.2xlarge m8g.2xlarge m8i.2xlarge m7a.2xlarge m7g.2xlarge m7i.2xlarge m6a.2xlarge m6g.2xlarge m6i.2xlarge"
 
@@ -11,10 +13,7 @@ do
 	for ins in ${instance_types} 
 	do
 		## 创建实例、安装软件
-		echo "$0: OS_TYPE=${os}, INSTANCE_TYPE=${ins}"
-		bash launch-instances-single.sh -s specjbb15 -t ${ins} -o ${os}
-		bash launch-instances-single.sh -s ffmpeg    -t ${ins} -o ${os}
-		bash launch-instances-single.sh -s spark     -t ${ins} -o ${os}
-		bash launch-instances-single.sh -s pts       -t ${ins} -o ${os}
+		echo "$0: SUT_NAME=${sut_name}, OS_TYPE=${os}, INSTANCE_TYPE=${ins} ..."
+		bash launch-instances-single.sh -s ${sut_name} -t ${ins} -o ${os}
 	done
 done
