4f50c275b416938212b77720d1f92059c025de2c
eric-yq <yuanquan8261@163.com>
2026-03-12 03:02:49 +0000

    change

--- specjbb15/tf_cfg_template/main.tf
+++ specjbb15/tf_cfg_template/main.tf
@@ -17,9 +17,9 @@ resource "aws_instance" "sut_server" {
   instance_type 			= var.instance_type
   count 					= var.number_of_instances
   ami 						= var.ami_id
-  subnet_id					= var.subnet_id
+#  subnet_id					= var.subnet_id
   vpc_security_group_ids 	= var.vpc_security_group_ids
-  placement_group           = var.placement_group_name
+#  placement_group           = var.placement_group_name
   key_name 					= var.ami_key_pair_name
   user_data 				= file(var.userdata_file)
 
