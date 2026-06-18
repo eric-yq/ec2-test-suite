120c2a0840696a2520079595aa8222f7af6a70c1
eric-yq <yuanquan8261@163.com>
2026-06-18 06:25:37 +0000

    change

--- blank/tf_cfg_template/userdata.sh
+++ blank/tf_cfg_template/userdata.sh
@@ -5,3 +5,6 @@ echo "This is a blank EC2 instance userdata script. No actions performed."
 ## 暂时关闭补丁更新流程
 sudo systemctl stop amazon-ssm-agent
 sudo systemctl disable amazon-ssm-agent
+
+cd /root/
+git clone https://github.com/eric-yq/ec2-test-suite.git
