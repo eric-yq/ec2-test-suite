7f232b09ac18485e829f6736322f571f5af353b9
eric-yq <yuanquan8261@163.com>
2026-09-16 00:06:06 +0000

    change

--- submit-benchmark-redis.sh
+++ submit-benchmark-redis.sh
@@ -3,7 +3,7 @@
 # Redis Benchmark
 # 待测 EC2 规格和 OS
 os_types="al2023"
-instance_types="$1"
+# instance_types="$1"
 instance_types="r9g.2xlarge r8a.2xlarge r8g.2xlarge r8i.2xlarge r7a.2xlarge r7g.2xlarge r7i.2xlarge r6a.2xlarge r6g.2xlarge r6i.2xlarge"
 # instance_types="m8a.2xlarge m8g.2xlarge m8i.2xlarge m7a.2xlarge m7g.2xlarge m7i.2xlarge m6a.2xlarge m6g.2xlarge m6i.2xlarge"
 
