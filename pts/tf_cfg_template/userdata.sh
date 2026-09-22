140d172b2499c8ec294613a32af3a2a33d3cc7a6
eric-yq <yuanquan8261@163.com>
2026-09-22 12:29:31 +0000

    change

--- pts/tf_cfg_template/userdata.sh
+++ pts/tf_cfg_template/userdata.sh
@@ -186,7 +186,7 @@ echo "[INFO] Step1: Start to perform PTS tests ..."
 tests="gmpbench primesieve stream cachebench ramspeed compress-zstd compress-lz4 blosc \
   botan john-the-ripper cython-bench ffmpeg x264 x265 tjbench vvenc blogbench nginx \
   graphics-magick smallpt draco renaissance dacapobench java-scimark2 scimark2 \
-  redis memtier-benchmark valkey keydb dragonflydb pogocache tidb etcd sonicjson simdjson \
+  redis memtier-benchmark valkey keydb dragonflydb pogocache tidb sonicjson simdjson \
   cassandra scylladb mariadb rocksdb influxdb clickhouse duckdb leveldb cockroach couchdb \
   stockfish mt-dgemm perf-bench mlpack mnn whisper-cpp whisperfile opencv \
   "
