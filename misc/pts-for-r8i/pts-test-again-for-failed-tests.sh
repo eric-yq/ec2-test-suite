e96ddb48a736bb8a3112d582129ff1aecdda8c26
eric-yq <yuanquan8261@163.com>
2026-09-24 13:19:28 +0000

    change

--- misc/pts-for-r8i/pts-test-again-for-failed-tests.sh
+++ misc/pts-for-r8i/pts-test-again-for-failed-tests.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-## 使用场景：已经完整过执行了一次PTS多项目测试，但是有一些 test 失败了，环境还在。
+######################################################################
+## 使用场景：已经完整过执行了一次PTS多项目测试，但是有一些 test 失败了，重新执行。
+## 并将某个testname的结果追加到某个已知的结果集。
+## 例如：2609235-NE-R6A4XLARG84
+## results="$1"
+######################################################################
 
 ## 查询 pts-result 目录下已经执行完成的日志文件中的错误信息，
 # grep -iE "err|fail" *.txt 
 # 然后逐个分析是否需要再次执行。
 
-##########################################################################
-# 需要重新测试的 test 项目，通过脚本执行时的参数带入，例如 "botan nginx scylladb"
-# tests="$@"
-##########################################################################
-
 cd /root/
 source /root/.bash_profile
 echo "yuanquan: TEST_RESULTS_IDENTIFIER=${PN}, TEST_RESULTS_DESCRIPTION=${PN}, TEST_RESULTS_NAME=${PN}"
 
-## 开始执行
-echo "[INFO][$(date +%Y%m%d%H%M%S)] StepX: Start to test some FAILED TESTS: ${tests} ..."
-echo "[INFO][$(date +%Y%m%d%H%M%S)] Followinig tests will be re-run ... " >> ${DATA_DIR}/pts-result-url-summary.txt
-
-# 关闭 checksum
-export NO_FILE_HASH_CHECKS=1
-
 ## 执行基准测试(标准)
 echo "[INFO] Step1: Start to perform PTS tests ..."
 
-tests="gmpbench primesieve stream cachebench ramspeed compress-zstd compress-lz4 blosc \
-  botan john-the-ripper cython-bench ffmpeg x264 x265 tjbench vvenc blogbench nginx \
-  graphics-magick smallpt draco renaissance dacapobench java-scimark2 scimark2 \
-  redis memtier-benchmark valkey keydb dragonflydb pogocache tidb sonicjson simdjson \
-  cassandra scylladb mariadb rocksdb influxdb clickhouse duckdb leveldb cockroach couchdb \
-  stockfish mt-dgemm perf-bench mlpack mnn whisper-cpp whisperfile opencv \
-  "
+tests="ffmpeg"
 for testname in ${tests} 
 do
     # 启动一个监控
@@ -39,47 +26,8 @@ do
     dool --cpu --sys --mem --net --net-packets --disk --io --proc-count --time --bits 30 > ${DOOL_FILE} 2>&1 &
     DOOL_PID=$!
     # 执行基准测试
-    FORCE_TIMES_TO_RUN=3 phoronix-test-suite batch-benchmark ${testname} > ${PTS_RESULT_DIR}/${testname}.txt
-    # 保存结果 URL
-    echo "${testname}:" >> ${DATA_DIR}/test-report-url-summary.txt
-    phoronix-test-suite info ${testname} | grep "Description: "  >> ${DATA_DIR}/test-report-url-summary.txt
-    grep "Results Uploaded To" ${PTS_RESULT_DIR}/${testname}.txt >> ${DATA_DIR}/test-report-url-summary.txt
-    # 停止监控
-    kill -9 ${DOOL_PID}
-
-    sleep 5
-done
-
-## 执行时间太长的，设置为只执行 1 次的tests:
-tests1="openssl pyperformance cpp-perf-bench c-ray lczero arrayfire hpcg quantlib"
-for testname in ${tests1} 
-do
-    # 启动一个监控
-    DOOL_FILE="${PTS_RESULT_DIR}/${testname}-dool.txt"
-    dool --cpu --sys --mem --net --net-packets --disk --io --proc-count --time --bits 60 > ${DOOL_FILE} 2>&1 &
-    DOOL_PID=$!
-    # 执行基准测试
-    FORCE_TIMES_TO_RUN=1 phoronix-test-suite batch-benchmark ${testname} > ${PTS_RESULT_DIR}/${testname}.txt
-    # 保存结果 URL
-    echo "${testname}:" >> ${DATA_DIR}/test-report-url-summary.txt
-    phoronix-test-suite info ${testname} | grep "Description: "  >> ${DATA_DIR}/test-report-url-summary.txt
-    grep "Results Uploaded To" ${PTS_RESULT_DIR}/${testname}.txt >> ${DATA_DIR}/test-report-url-summary.txt
-    # 停止监控
-    kill -9 ${DOOL_PID}
-
-    sleep 5
-done
-
-## 特殊任务：执行2次的tests。
-tests2="scikit-learn"
-for testname in ${tests2} 
-do
-    # 启动一个监控
-    DOOL_FILE="${PTS_RESULT_DIR}/${testname}-dool.txt"
-    dool --cpu --sys --mem --net --net-packets --disk --io --proc-count --time --bits 60 > ${DOOL_FILE} 2>&1 &
-    DOOL_PID=$!
-    # 执行基准测试
-    FORCE_TIMES_TO_RUN=2 phoronix-test-suite batch-benchmark ${testname} > ${PTS_RESULT_DIR}/${testname}.txt
+    FORCE_TIMES_TO_RUN=3 phoronix-test-suite batch-benchmark ${testname} --result-file=${results} \
+      > ${PTS_RESULT_DIR}/${testname}.txt
     # 保存结果 URL
     echo "${testname}:" >> ${DATA_DIR}/test-report-url-summary.txt
     phoronix-test-suite info ${testname} | grep "Description: "  >> ${DATA_DIR}/test-report-url-summary.txt
@@ -98,12 +46,14 @@ ls -ltr ${PTS_RESULT_DIR} >> ${DATA_DIR}/pts-list-installed-tests.txt
 df -h  >> ${DATA_DIR}/pts-list-installed-tests.txt
 rm -rf ${LOG_DIR}/*
 cp -r /var/log/cloud-init*.log /var/log/phoronix-test-suite-*.log /var/lib/cloud/ /root/userdata.sh ${LOG_DIR}
-tar czfP ${DATA_DIR}-all.tar.gz ${DATA_DIR}
+DATATIME=$(date +%Y%m%d.%H%M%S)
+tar czfP ${DATA_DIR}-fix-failed-tests-${DATATIME}.tar.gz ${DATA_DIR}
 aws_s3_bucket_name=$(aws s3 ls | awk '{print $3}' | grep ec2-core-benchmark | head -1)
-aws s3 cp ${DATA_DIR}-all.tar.gz s3://${aws_s3_bucket_name}/result_pts/ && \
+aws s3 cp ${DATA_DIR}-fix-failed-tests-${DATATIME}.tar.gz s3://${aws_s3_bucket_name}/result_pts/ && \
 echo "[INFO] Step3: Result files have been uploaded to s3 bucket. BYE BYE."
 
-## Disable 服务，这样 reboot 后不会再次执行
+# Disable 服务，这样 reboot 后不会再次执行
+systemctl stop    userdata.service
 systemctl disable userdata.service
 
 ## 停止实例
